719cbf4bf1841c8c0e9126a40710c4f1c6065894
Andrey Bychko <courage.andrey@gmail.com>
2017-11-21 22:38:22 +0300

    Добавлен проект с функциональными тестами.

---
+++ Code/UnitTests/EnumerationsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+using ComfortIsland.Database;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class EnumerationsTest
+	{
+		[TestMethod]
+		public void DocumentType()
+		{
+			var values = Enum.GetValues(typeof(DocumentType)).OfType<DocumentType>().ToList();
+			Assert.IsTrue(values.SequenceEqual(DocumentTypeImplementation.AllTypes.Keys));
+		}
+	}
+}
