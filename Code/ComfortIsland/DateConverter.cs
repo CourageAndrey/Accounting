82ae4c063134c3eae36d3e9f208b2ec925c7d7cf
Andrey Bychko <courage.andrey@gmail.com>
2017-12-09 21:31:17 +0300

    Корректно настроено форматирование дат.

---
+++ Code/ComfortIsland/DateConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+using System.Windows.Data;
+
+namespace ComfortIsland
+{
+	public class DateConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var timestamp = (DateTime) value;
+			return string.Format(CultureInfo.InvariantCulture, "{0}, {1}", timestamp.ToLongDateString(), timestamp.ToShortTimeString());
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
