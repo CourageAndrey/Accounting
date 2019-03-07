964d3a3b0d0996952d31273182cc6e5ce5d97d8f
Andrei Bychko <abychko@aras.com>
2019-03-07 10:47:57 +0300

    Включено журналирование возникающих ошибок.

--- Code/ComfortIsland/App.xaml.cs
+++ Code/ComfortIsland/App.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Text;
 using System.Windows;
 
 namespace ComfortIsland
@@ -13,5 +11,38 @@ namespace ComfortIsland
 	/// </summary>
 	public partial class App : Application
 	{
+		public App()
+		{
+			DispatcherUnhandledException += (sender, exceptionArgs) =>
+			{
+				LogError("Application.DispatcherUnhandledException", exceptionArgs.Exception);
+			};
+			AppDomain.CurrentDomain.UnhandledException += (sender, exceptionArgs) =>
+			{
+				var error = exceptionArgs.ExceptionObject as Exception;
+				LogError(
+					"AppDomain.CurrentDomain.UnhandledException",
+					error ?? new Exception("" + exceptionArgs.ExceptionObject));
+			};
+		}
+
+		private static void LogError(string source, Exception error)
+		{
+			var text = new StringBuilder();
+			text.AppendLine("================================");
+			text.AppendLine(source);
+			text.AppendLine(DateTime.Now.ToString("F"));
+
+			do
+			{
+				text.AppendLine();
+				text.AppendLine(error.GetType().FullName);
+				text.AppendLine(error.Message);
+				text.AppendLine(error.StackTrace);
+				error = error.InnerException;
+			} while (error != null);
+
+			File.AppendAllText("Exception.txt", text.ToString());
+		}
 	}
 }
