4d92c31ad27a2725ae2c69574c29dd06e65f3c40
Andrey Bychko <bublic-xiii@list.ru>
2019-07-01 22:35:11 +0300

    Исправлено отображение дерева производимой продукции.

---
+++ Code/ComfortIsland/ViewModels/PositionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Markup;
+
+namespace ComfortIsland.ViewModels
+{
+	public class PositionType : MarkupExtension
+	{
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return typeof(KeyValuePair<BusinessLogic.Product, double>);
+		}
+	}
+}
