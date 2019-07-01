2ed5219ef297190f69482adbda46635dabc44a18
Andrey Bychko <bublic-xiii@list.ru>
2019-07-01 22:40:49 +0300

    Добавлен тест на перечисление DocumentState.

--- Code/UnitTests/EnumerationsTest.cs
+++ Code/UnitTests/EnumerationsTest.cs
@@ -14,5 +14,12 @@ namespace UnitTests
 			var values = Enum.GetValues(typeof(ComfortIsland.Xml.DocumentType)).OfType<ComfortIsland.Xml.DocumentType>().ToList();
 			Assert.IsTrue(values.SequenceEqual(ComfortIsland.BusinessLogic.DocumentType.AllTypes.Keys));
 		}
+
+		[TestMethod]
+		public void DocumentState()
+		{
+			var values = Enum.GetValues(typeof(ComfortIsland.Xml.DocumentState)).OfType<ComfortIsland.Xml.DocumentState>().ToList();
+			Assert.IsTrue(values.SequenceEqual(ComfortIsland.BusinessLogic.DocumentState.AllStates.Keys));
+		}
 	}
 }
