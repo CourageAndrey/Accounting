6199501163dc68925bae9df6532db3d513f0724d
Andrei Bychko <abychko@aras.com>
2019-07-02 14:27:25 +0300

    Откат документов покрыт тестами.

--- Code/UnitTests/WorkflowTest.cs
+++ Code/UnitTests/WorkflowTest.cs
@@ -120,6 +120,116 @@ namespace UnitTests
 			Assert.AreEqual(35, database.Balance[productParent.ID]);
 		}
 
+		[TestMethod]
+		public void RollbackIncomeDecreasesBalance()
+		{
+			// arrange
+			Product productChild1, productChild2, productParent;
+			var database = createTestBase(out productChild1, out productChild2, out productParent);
+			var document = new Document(DocumentType.Income)
+			{
+				ID = 0,
+				Date = new DateTime(),
+				Number = string.Empty,
+				Positions = new Dictionary<Product, double>
+				{
+					{ productChild1, 1 },
+					{ productChild2, 2 },
+					{ productParent, 3 },
+				}
+			};
+
+			// act
+			document.Rollback(database);
+
+			// assert
+			Assert.AreEqual(9, database.Balance[productChild1.ID]);
+			Assert.AreEqual(18, database.Balance[productChild2.ID]);
+			Assert.AreEqual(27, database.Balance[productParent.ID]);
+		}
+
+		[TestMethod]
+		public void RollbackOutcomeIncreasesBalance()
+		{
+			// arrange
+			Product productChild1, productChild2, productParent;
+			var database = createTestBase(out productChild1, out productChild2, out productParent);
+			var document = new Document(DocumentType.Outcome)
+			{
+				ID = 0,
+				Date = new DateTime(),
+				Number = string.Empty,
+				Positions = new Dictionary<Product, double>
+				{
+					{ productChild1, 1 },
+					{ productChild2, 2 },
+					{ productParent, 3 },
+				}
+			};
+
+			// act
+			document.Rollback(database);
+
+			// assert
+			Assert.AreEqual(11, database.Balance[productChild1.ID]);
+			Assert.AreEqual(22, database.Balance[productChild2.ID]);
+			Assert.AreEqual(33, database.Balance[productParent.ID]);
+		}
+
+		[TestMethod]
+		public void RollbackWarehouseIncreasesBalance()
+		{
+			// arrange
+			Product productChild1, productChild2, productParent;
+			var database = createTestBase(out productChild1, out productChild2, out productParent);
+			var document = new Document(DocumentType.ToWarehouse)
+			{
+				ID = 0,
+				Date = new DateTime(),
+				Number = string.Empty,
+				Positions = new Dictionary<Product, double>
+				{
+					{ productChild1, 1 },
+					{ productChild2, 2 },
+					{ productParent, 3 },
+				}
+			};
+
+			// act
+			document.Rollback(database);
+
+			// assert
+			Assert.AreEqual(11, database.Balance[productChild1.ID]);
+			Assert.AreEqual(22, database.Balance[productChild2.ID]);
+			Assert.AreEqual(33, database.Balance[productParent.ID]);
+		}
+
+		[TestMethod]
+		public void RollbackProduceChangesBalance()
+		{
+			// arrange
+			Product productChild1, productChild2, productParent;
+			var database = createTestBase(out productChild1, out productChild2, out productParent);
+			var document = new Document(DocumentType.Produce)
+			{
+				ID = 0,
+				Date = new DateTime(),
+				Number = string.Empty,
+				Positions = new Dictionary<Product, double>
+				{
+					{ productParent, 5 },
+				}
+			};
+
+			// act
+			document.Rollback(database);
+
+			// assert
+			Assert.AreEqual(15, database.Balance[productChild1.ID]);
+			Assert.AreEqual(30, database.Balance[productChild2.ID]);
+			Assert.AreEqual(25, database.Balance[productParent.ID]);
+		}
+
 		private static Database createTestBase(out Product productChild1, out Product productChild2, out Product productParent)
 		{
 			var unit = new Unit
