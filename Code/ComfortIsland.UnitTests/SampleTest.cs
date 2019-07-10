c3ea086608e83a0c146fffe718957855f3916ddd
Andrei Bychko <abychko@aras.com>
2019-07-10 09:19:13 +0300

    Добавлен проект функциональных тестов NUnit.

---
+++ Code/ComfortIsland.UnitTests/SampleTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ComfortIsland.UnitTests
+{
+	public class SampleTest
+	{
+		[SetUp]
+		public void SetupTest()
+		{ }
+
+		[Test]
+		public void HelloWorld()
+		{ }
+	}
+}
