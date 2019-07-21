865e58c1af7f85ed7f00fb354c3fc97b56e63bed
Andrey Bychko <bublic-xiii@list.ru>
2019-07-21 23:02:13 +0300

    Добавлен проект тестов пользовательского интерфейса.

---
+++ Code/ComfortIsland.UiTests/SmokeTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+using ComfortIsland.UiTests.Framework;
+
+namespace ComfortIsland.UiTests
+{
+	public class SmokeTest
+	{
+		[Test]
+		public void ApplicationLaunchIsSuccessfull()
+		{
+			// arrange
+			var application = UI.Run(() => new App { MainWindow = new MainWindow() });
+			var mainWindow = application.Get(x => x.MainWindow);
+
+			// act
+			application.Invoke(x => x.Shutdown());
+
+			// assert
+			Assert.IsNotNull(mainWindow);
+		}
+	}
+}
