08317b25bb18da17a03035ff162aae582ae6482f
Andrei Bychko <abychko@aras.com>
2019-07-25 16:02:53 +0300

    Класс DateConverter покрыт функциональными тестами.

---
+++ Code/ComfortIsland.UnitTests/Helpers/DateConverterTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+using NUnit.Framework;
+
+using ComfortIsland.Helpers;
+
+namespace ComfortIsland.UnitTests.Helpers
+{
+	public class DateConverterTest
+	{
+		[Test]
+		public void ConvertAcceptDateTimeOnly()
+		{
+			// arrange
+			var converter = new DateConverter();
+
+			// act and assert
+			Assert.Throws<InvalidCastException>(() => converter.Convert(
+				string.Empty,
+				typeof(string),
+				null,
+				CultureInfo.InvariantCulture));
+		}
+
+		[Test]
+		public void HappyPath()
+		{
+			// arrange
+			var converter = new DateConverter();
+
+			// act
+			var date = DateTime.Now;
+			string dateString = (string) converter.Convert(
+				date,
+				typeof(DateTime),
+				null,
+				CultureInfo.InvariantCulture);
+
+			// assert
+			Assert.IsTrue(dateString.Contains(date.ToLongDateString()));
+			Assert.IsTrue(dateString.Contains(date.ToLongDateString()));
+		}
+
+		[Test]
+		public void ConvertBackIsNotSupported()
+		{
+			// arrange
+			var converter = new DateConverter();
+
+			// act and assert
+			Assert.Throws<NotSupportedException>(() => converter.ConvertBack(
+				string.Empty,
+				typeof(DateTime),
+				null,
+				CultureInfo.InvariantCulture));
+		}
+	}
+}
