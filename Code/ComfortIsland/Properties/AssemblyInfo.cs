01f859f88d948db1164d71b382ef8bfc2b4c9131
Andrey Bychko <bublic-xiii@list.ru>
2019-08-11 13:56:36 +0300

    internal-код приложения сделан видимым для библиотеки UI-тестов.

--- Code/ComfortIsland/Properties/AssemblyInfo.cs
+++ Code/ComfortIsland/Properties/AssemblyInfo.cs
@@ -19,6 +19,7 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None,ResourceDictionaryLocation.SourceAssembly)]
 
 [assembly: InternalsVisibleTo("ComfortIsland.UnitTests")]
+[assembly: InternalsVisibleTo("ComfortIsland.UiTests")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
