ebd484ea49696c81a0077ea52aceb7779551cdda
Andrey Bychko <bublic-xiii@list.ru>
2019-12-03 19:41:04 +0300

    Класс DatabaseDriverExtensions вынесен в отдельный файл.

---
+++ Code/Accounting.Core/DataAccessLayer/DatabaseDriverExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ComfortIsland.BusinessLogic;
+
+namespace Accounting.Core.DataAccessLayer
+{
+	public static class DatabaseDriverExtensions
+	{
+		public static Database TryLoad(this IDatabaseDriver databaseDriver)
+		{
+			Database database;
+			if (databaseDriver.CanLoad)
+			{
+				database = databaseDriver.Load();
+			}
+			else
+			{
+				database = Database.CreateDefault();
+				databaseDriver.Save(database);
+			}
+			return database;
+		}
+	}
+}
--- Code/Accounting.Core/DataAccessLayer/IDatabaseDriver.cs
+++ Code/Accounting.Core/DataAccessLayer/IDatabaseDriver.cs
@@ -10,22 +10,4 @@ namespace Accounting.Core.DataAccessLayer
 
 		void Save(Database database);
 	}
-
-	public static class DatabaseDriverExtensions
-	{
-		public static Database TryLoad(this IDatabaseDriver databaseDriver)
-		{
-			Database database;
-			if (databaseDriver.CanLoad)
-			{
-				database = databaseDriver.Load();
-			}
-			else
-			{
-				database = Database.CreateDefault();
-				databaseDriver.Save(database);
-			}
-			return database;
-		}
-	}
 }
