ff9d7175ffaf259c2bc4c6e16ac664edc83c2fc5
Andrey Bychko <bublic-xiii@list.ru>
2019-12-20 20:19:43 +0300

    OpenXML reports refactoring: Убран using типа Border.

--- Code/Accounting.Reports.OpenXml/ExcelOpenXmlReportExporter.cs
+++ Code/Accounting.Reports.OpenXml/ExcelOpenXmlReportExporter.cs
@@ -2,16 +2,16 @@
 using System.IO;
 using System.Linq;
 
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
 using Ap = DocumentFormat.OpenXml.ExtendedProperties;
 using Vt = DocumentFormat.OpenXml.VariantTypes;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Spreadsheet;
 using X15ac = DocumentFormat.OpenXml.Office2013.ExcelAc;
 using X15 = DocumentFormat.OpenXml.Office2013.Excel;
 using X14 = DocumentFormat.OpenXml.Office2010.Excel;
 using A = DocumentFormat.OpenXml.Drawing;
-using Border = DocumentFormat.OpenXml.Spreadsheet.Border;
 using Thm15 = DocumentFormat.OpenXml.Office2013.Theme;
 
 using Accounting.Core.Application;
