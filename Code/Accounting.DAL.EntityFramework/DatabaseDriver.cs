0d2dd0a6ef838f9a1df3b3408f6e930111a78418
Andrei Bychko <abychko@aras.com>
2019-12-23 11:45:19 +0300

    Добавлена модель данных для доступа к БД EntityFramework.

--- Code/Accounting.DAL.EntityFramework/DatabaseDriver.cs
+++ Code/Accounting.DAL.EntityFramework/DatabaseDriver.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using Accounting.Core.Application;
+using Accounting.Core.BusinessLogic;
+using Accounting.DAL.EntityFramework.Entities;
 
 namespace Accounting.DAL.EntityFramework
 {
@@ -16,12 +20,79 @@ namespace Accounting.DAL.EntityFramework
 		public bool CanLoad
 		{ get { throw new NotImplementedException(); } }
 
-		public Core.BusinessLogic.Database Load()
+		public Database Load()
 		{
-			throw new NotImplementedException();
+			using (var database = new AccountingEntities())
+			{
+				var units = database.Units.ToDictionary(
+					u => u.ID,
+					u => new Core.BusinessLogic.Unit
+					{
+						ID = u.ID,
+						Name = u.Name,
+						ShortName = u.ShortName,
+					});
+
+				var products = database.Products.ToDictionary(
+					p => p.ID,
+					p => new Core.BusinessLogic.Product
+					{
+						ID = p.ID,
+						Name = p.Name,
+						Unit = units[p.UnitID],
+					});
+				foreach (var part in database.IsPartOfs)
+				{
+					products[part.ParentID].Children.Add(products[part.ChildID], part.Count);
+				}
+
+				var balance = database.Balances.ToDictionary(
+					b => b.ProductID,
+					b => b.Count);
+
+				var documentTypes = new Dictionary<short, DocumentType>
+				{
+					{ 0, DocumentType.Income },
+					{ 1, DocumentType.Outcome },
+					{ 2, DocumentType.Produce },
+					{ 3, DocumentType.ToWarehouse },
+				};
+				var documentStates = new Dictionary<short, DocumentState>
+				{
+					{ 0, DocumentState.Active },
+					{ 1, DocumentState.Edited },
+					{ 2, DocumentState.Deleted },
+				};
+				var documents = database.Documents.ToDictionary(
+					d => d.ID,
+					d =>
+					{
+						var document = new Core.BusinessLogic.Document(
+							d.PreviousVersionID,
+							documentTypes[d.TypeID],
+							documentStates[d.StateID])
+						{
+							ID = d.ID,
+							Number = d.Number,
+							Date = d.Date,
+						};
+						foreach (var position in d.Positions)
+						{
+							document.Positions[products[position.ProductID]] = position.Count;
+						}
+						return document;
+					});
+
+
+				return new Database(
+					units.Values,
+					products.Values,
+					balance,
+					documents.Values);
+			}
 		}
 
-		public void Save(Core.BusinessLogic.Database database)
+		public void Save(Database database)
 		{
 			throw new NotImplementedException();
 		}
